7f3838898fa378ec40e080e531f609f03cb65071
Alvaro <alvarogonjim95@gmail.com>
2020-01-16 18:09:14 +0100

    Add the assembler and add the rest of arguments to call the SNP

--- snp.py
+++ snp.py
@@ -2,20 +2,23 @@
 import getopt, sys, data, logging
 import numpy as np
 from collections import Counter
-
-# include standard modules
+import assembler
+from Bio import pairwise2
+from Bio.pairwise2 import format_alignment
 import argparse
+import math
 
 class Snp():
-    def __init__(self, kmers):
+    def __init__(self, kmers_0, kmers_1):
         '''
 
         :param C: Coverage value
-        :param kmers: Pickle file of k-mers
+        :param kmers_0: Pickle file of k-mers 0
+        :param kmers_1: Pickle file of k-mers 1
         '''
-        logging.debug('Initializing SNP and loading the kmers from: ' + str(kmers))
-        self.kmers = data.load_obj(kmers)
-        self.hist_kmers = dict(Counter(kmers.values()))
+        self.kmers = data.load_obj(kmers_0)
+        self.kmers_1 = data.load_obj(kmers_1)
+        self.hist_kmers = dict(Counter(kmers_0.values()))
 
     def mean_coverage(self, start_value=5):
         '''
@@ -49,48 +52,88 @@ class Snp():
         '''
         return np.exp(-l) * np.sum([l ** i / np.math.factorial(i) for i in range(0, k + 1)])
 
-    def snp_with_cdf(self, error_rate=0.01):
+    def snp_with_cdf(self, cmean, error_rate=0.01):
         '''
 
-        :param error_rate: Minimum error rate to add the SNP
-        :return: array of SNPs
+        @param kmers_0: Dictionary of kmers to find SNPs
+        @param kmers_1: Dictionary of kmers to find
+        @param cmean: Mean coverage for the corresponding kmer
+        @param error_rate: Minimum error rate to add the SNP
+        @return: array of SNPs
         '''
-        kmers_snp = []
+        kmers_0_snp = []
         for kmer in self.kmers.keys():
-            if kmer not in self.kmers.keys() and self.cdf_poisson(self.mean_coverage(), self.kmers[kmer]) > error_rate:
-                kmers_snp = np.append(kmers_snp, kmer)
-        return kmers_snp
+            if kmer not in self.kmers_1.keys() and self.cdf_poisson(cmean, self.kmers[kmer]) > error_rate:
+                kmers_0_snp.append(kmer)
+        return kmers_0_snp
 
 if __name__ == '__main__':
     # auxiliar variables to store the path for k-mers files and name for SNPs
+    kmers_0 = None
     kmers_1 = None
+    snp0_name = None
     snp1_name = None
-
+    k = None
     # define the program description
-    text = 'This script will calculate the SNPs from two differents k-mers files'
+    text = 'This script will calculate the SNPs from a k-mers files'
 
     # initiate the parser with a description
     parser = argparse.ArgumentParser(description=text)
-    parser.add_argument("-kmers", "--kmers_file", help="Path of the k-mers file")
-    parser.add_argument("-snp", "--snp_name", help="Name to store the SNPs")
+    parser.add_argument("-kmers", "--kmers_file", help="Path of the first k-mers file", required=True)
+    parser.add_argument("-kmers2", "--kmers_file2", help="Path of the second k-mers file", required=True)
+    parser.add_argument("-snp", "--snp_name", help="Name of the first SNPs file", required=False, default='snp1')
+    parser.add_argument("-snp2", "--snp_name2", help="Name of the second SNPs file", required=False, default='snp2')
+    parser.add_argument("-k", help="K value", required=False, default=15)
 
     args = parser.parse_args()
     try:
         if args.kmers:
-            logging.info('Getting the path of k-mer file')
-            kmers_1 = args.kmers
+            logging.debug('Getting the path of k-mer file')
+            kmers_0 = args.kmers
+        if args.kmers2:
+            logging.debug('Getting the path of the second  k-mer file')
+            kmers_1 = args.kmers2
         if args.snp:
-            logging.info('Getting the name to save the SNPs')
-            snp1_name = args.snp
-
+            logging.debug('Getting the name to save the SNPs')
+            snp0_name = args.snp
+        if args.snp2:
+            logging.debug('Getting the name to save the second SNPs')
+            snp1_name = args.snp2
+        if args.k:
+            logging.debug('Getting the k value')
+            k = args.k
         if (kmers_1 and snp1_name):
             logging.debug('Creating the Snp objects')
-            snp_1 = Snp(kmers_1)
+            snp_0 = Snp(kmers_0=kmers_0, kmers_1=kmers_1)
+            snp_1 = Snp(kmers_0=kmers_1, kmers_1=kmers_0)
 
             logging.debug('Get the SNPs from the first k-mers file')
-            kmers_1_snps = snp_1.snp_with_cdf()
+            kmers_0_snp = snp_0.snp_with_cdf(cmean=snp_0.mean_coverage())
             logging.info('Save the SNPs from the first k-mers')
-            data.save_obj(kmers_1_snps, snp1_name)
+            data.save_obj(kmers_0_snp,snp0_name)
+
+            logging.debug('Get the SNPs from the second k-mers file')
+            kmers_1_snp = snp_1.snp_with_cdf(cmean=snp_1.mean_coverage())
+            logging.info('Save the SNPs from the second k-mers')
+            data.save_obj(kmers_1_snp, snp1_name)
+
+            logging.info('Printing the list of SNPs')
+
+            for i in range(math.floor(len(kmers_0_snp) / k)):
+                print('SNP ' + str(i + 1))
+                # Define two sequences to be aligned
+                X = assembler.ah(kmers_0_snp[round(len(kmers_0_snp) / math.floor(len(kmers_0_snp) / k) * i):round(
+                    len(kmers_0_snp) / math.floor(len(kmers_0_snp) / k) * (i + 1))])
+                Y = assembler.ah(kmers_1_snp[round(len(kmers_1_snp) / math.floor(len(kmers_1_snp) / k) * i):round(
+                    len(kmers_1_snp) / math.floor(len(kmers_1_snp) / k) * (i + 1))])
+
+                # Get a list of the global alignments between the two sequences ACGGGT and ACG
+                # No parameters. Identical characters have score of 1, else 0.
+                # No gap penalties.
+                alignments = pairwise2.align.globalms(X, Y, 2, 0, -1, 0)
+                # Use format_alignment method to format the alignments in the list
+                print(format_alignment(*alignments[0]))
+
         else:
             logging.error('Use -h to see how to use the script')
             sys.exit(2)
