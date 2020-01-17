76169bbc9b80e6ea6930305e54cb6150e7928d3e
Alvaro <alvarogonjim95@gmail.com>
2020-01-17 15:58:12 +0100

    Adding the argparser for the terminal and store the dictionary

--- kmers.py
+++ kmers.py
@@ -1,9 +1,11 @@
-import data
+from Bio import SeqIO
+import argparse, os, logging, data
 
 class Kmers:
     def __init__(self, reads, k=15):
+        dirpath = os.getcwd()
         self.k = k
-        self.reads = data.load_obj(reads)
+        self.reads = list(SeqIO.parse(dirpath + '/' + reads, "fasta"))
 
     def k_mer_counting(self):
         """
@@ -21,3 +23,24 @@ class Kmers:
                 kmers[kmer] = kmers.get(kmer, 0) + 1
         return kmers
 
+if __name__ == '__main__':
+
+    #  # define the program description
+    text = "This tool takes a simple FASTA files and compute the k-mers algorithm."
+
+    # initiate the parser with a description
+    parser = argparse.ArgumentParser(description=text)
+    parser.add_argument(
+        "-i", "--fasta", help="Name of the FASTA file", required=True
+    )
+    parser.add_argument(
+        "-o", "--output", help="Name of the file to store the k-mers", required=False, default = 'kmers_0'
+    )
+    parser.add_argument("-k", help="K value", required=False, default=15)
+    args = parser.parse_args()
+
+    print('Initializing')
+    kmer = Kmers(args.fasta, args.k)
+    dictionary_kmers =  kmer.k_mer_counting()
+    print('Storing the dictionary of k-mers')
+    data.save_obj(dictionary_kmers, args.output + '.pkl')
